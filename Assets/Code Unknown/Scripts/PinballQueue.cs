9abcd322b2cbc66593d0d1c6c50ed18bd5ddbd67
halohawaii <13061663804@163.com>
2025-10-22 22:02:34 -0400

    Add Dotween plugin and some logics in ending panel

--- Assets/Code Unknown/Scripts/PinballQueue.cs
+++ Assets/Code Unknown/Scripts/PinballQueue.cs
@@ -13,6 +13,9 @@ public class PinballQueue : MonoBehaviour
     [Tooltip("World coordinates for where to spawn the bottom-most ball")]
     [SerializeField] Vector3 spawnPosition = Vector3.zero;
 
+    [Tooltip("Finish Panel when Game Over")] 
+    [SerializeField] GameObject UIFinish;
+
     Queue<AbstractBall> ballQueue = new Queue<AbstractBall>();
     public static PinballQueue Instance { get; private set; }
     
@@ -91,6 +94,8 @@ public class PinballQueue : MonoBehaviour
         else
         {
             print("Game Over");
+            GameObject canvas = GameObject.FindWithTag("canvas");
+            var _UIFinishObj = Instantiate(UIFinish, canvas.transform, false);
         }
     }
 }
---
+++ Assets/Code Unknown/Scripts/UI/UIFinish.cs
@@ -0,0 +1,69 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+
+namespace UI
+{
+    public class UIFinish : MonoBehaviour
+    {
+        public TextMeshProUGUI terrainPointsText;
+        public TextMeshProUGUI monsterSlainText;
+        public TextMeshProUGUI goldEarnedText;
+        public TextMeshProUGUI ballUsedText;
+        public TextMeshProUGUI totalPointsText;
+        public TextMeshProUGUI damageDealtText;
+
+        public GameObject level1;
+        public GameObject UIMain;
+
+        private void Start()
+        {
+            ShowTexts();
+        }
+
+        public void OnReplayButtonClick()
+        {
+            var _level1 = GameObject.FindWithTag("level");
+            Destroy(_level1);
+            var machine = Instantiate(level1);
+
+            var uiMain = FindObjectOfType<UIGameMain>();
+            Destroy(uiMain.gameObject);
+            var uiupgrade = FindObjectOfType<UIUpgrade>();
+            if (uiupgrade != null)
+            {
+                Destroy(uiupgrade);
+            }
+            GameObject canvas = GameObject.FindWithTag("canvas");
+            var _UIMainObj = Instantiate(UIMain, canvas.transform, false);
+            Destroy(gameObject);
+        }
+
+        public void OnClickBackToTitle()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.name);
+        }
+
+        public void ShowTexts()
+        {
+            var monsterSlain = GameStatsManager.Instance.killCount;
+            string monsterSlainStr = "Monster Slain" + new string('.', 25 - 13 - monsterSlain.ToString().Length) + monsterSlain.ToString();
+            ShowTextAnim(monsterSlainText, monsterSlainStr, 3f);
+            
+            var totalPoint = GameStatsManager.Instance.score;
+            string totalPointStr = "Total Points" + new string('.', 25 - 12 - totalPoint.ToString().Length) + totalPoint.ToString();
+            ShowTextAnim(totalPointsText, totalPointStr, 3f);
+        }
+
+        void ShowTextAnim(TextMeshProUGUI textMesh, string content, float time)
+        {
+            textMesh.text = "";
+            textMesh.DOText(content, time);
+        }
+    }
+}
+
