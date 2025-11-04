493260f11b9a8f9b7717847714fbcf9671cd0be7
numbersguy104 <numbersboy@icloud.com>
2025-11-04 18:05:36 -0500

    Work on story UI screen
    Work on the story UI screen which can be accessed by clicking "Collection" on the main menu. Currently a work in progress

--- Assets/Code Unknown/Scripts/UI/UIStart.cs
+++ Assets/Code Unknown/Scripts/UI/UIStart.cs
@@ -6,17 +6,30 @@ namespace UI
     {
 
         public GameObject UIPrepare;
-
         private GameObject _UIPrepareObj;
+
+        public GameObject UIStory;
+        private GameObject _UIStoryObj;
         
         public void OnStartGameBtnClick()
         {
             if (_UIPrepareObj == null)
             {
-                Canvas canvas = FindObjectOfType<Canvas>();
+                Canvas canvas = FindAnyObjectByType<Canvas>();
                 _UIPrepareObj = Instantiate(UIPrepare, canvas.transform, false);
             }
             _UIPrepareObj.SetActive(true);
+            gameObject.SetActive(false);
+        }
+
+        public void OnCollectionBtnClick()
+        {
+            if (_UIStoryObj == null)
+            {
+                Canvas canvas = FindAnyObjectByType<Canvas>();
+                _UIStoryObj = Instantiate(UIStory, canvas.transform, false);
+            }
+            _UIStoryObj.SetActive(true);
             gameObject.SetActive(false);
         }
     }
