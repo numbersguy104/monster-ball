c613d3054e8dd390f70d5b7b105890fce3dce26d
Wildside3 <lij107230013@gmail.com>
2025-11-15 16:08:08 -0500

    half ball full ball complete

---
+++ Assets/Code Unknown/Scripts/Balls/HalfBall.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class HalfBall : AbstractAbilityBall
+{
+    [SerializeField] IceZone iceZonePrefab;
+    [Header("Ball Settings")]
+    [Tooltip("Ice zone duration")]
+    [SerializeField] private float zoneDuration = 5.0f;
+    [Tooltip("Ice zone size ralated to ball")]
+    [SerializeField] private float zoneScale = 1.0f;
+
+    protected override void Skill()
+    {
+        //CreateTrail();
+    }
+
+    protected override void Awake()
+    {
+        BallInit("HalfBall");
+        //zoneScale *= GameStatsManager.Instance.Iceball_ZoneSize;
+    }
+
+    private void CreateTrail()
+    {
+        var trail = Instantiate(iceZonePrefab, transform.position, transform.rotation);
+        trail.transform.up = GetVelocity();
+        float size = transform.lossyScale.x * zoneScale;
+        trail.transform.localScale = new Vector3(size, size, size);
+        trail.transform.localRotation = Quaternion.identity;
+        trail.GetComponent<IceZone>().SetDuration(zoneDuration);
+    }
+
+}
